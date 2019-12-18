f07cbf4f3c4cb64c16140ac48aefad66e75eaffb
Kahlil Beasley <ksbeasle@asu.edu>
2019-12-18 13:24:36 -0700

    .

--- SellerMS/src/main/java/com/icart/SellerMS/SellerMsApplication.java
+++ SellerMS/src/main/java/com/icart/SellerMS/SellerMsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SellerMsApplication {
-//
+
 	public static void main(String[] args) {
 		SpringApplication.run(SellerMsApplication.class, args);
 	}
